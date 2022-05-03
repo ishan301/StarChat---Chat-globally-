6eda0a4d5df14680b191105c1fe3ba079c67dbda
Ishan <ishankhandelwals@gmail.com>
2022-05-03 15:42:22 +0530

    minor changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -30,6 +31,6 @@ io.on('connection', (socket) => {
     });
   });
 
-server.listen(80, () => {
+server.listen(PORT, () => {
   console.log('listening on port 80');
 });
